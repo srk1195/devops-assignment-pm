d8b7be9189b65bb3839dd7c0bf5d69381fbb5c42
Sai Rahul Kodumuru <ksairahul15@gmail.com>
2022-02-03 23:23:42 -0400

    Created test-cases for class Car

--- test_app.py
+++ test_app.py
@@ -24,3 +24,53 @@ def testDirectionStopped():
 def testDirectionNotStopped():
     direction = Direction.STOPPED
     assert direction.value != 1
+
+# Testing the Car class
+def testCarInitialDirection():
+    mustang = Car()
+    assert mustang.direction == Direction.STOPPED
+
+def testIsMovingFalse():
+    mustang = Car()
+    assert mustang.isMoving() == False
+
+def testIsMovingTrue():
+    mustang = Car()
+    mustang.moveForward()
+    assert mustang.isMoving() == True
+
+def testIsMoveForwardTrue():
+    mustang = Car()
+    mustang.moveForward()
+    assert mustang.direction == Direction.FORWARD
+
+def testIsMoveForwardFalse():
+    mustang = Car()
+    mustang.moveForward()
+    assert mustang.direction != Direction.REVERSE
+    assert mustang.direction != Direction.STOPPED
+
+def testIsMoveBackwardTrue():
+    mustang = Car()
+    mustang.moveBackward()
+    assert mustang.direction == Direction.REVERSE
+
+def testIsMoveBackwardFalse():
+    mustang = Car()
+    mustang.moveBackward()
+    assert mustang.direction != Direction.FORWARD 
+    assert mustang.direction != Direction.STOPPED
+
+def testIsStopTrue():
+    mustang = Car()
+    mustang.moveBackward()
+    mustang.moveForward()
+    mustang.stop()
+    assert mustang.direction == Direction.STOPPED
+
+def testIsStopFalse():
+    mustang = Car()
+    mustang.moveBackward()
+    mustang.moveBackward()
+    mustang.stop()
+    assert mustang.direction != Direction.FORWARD
